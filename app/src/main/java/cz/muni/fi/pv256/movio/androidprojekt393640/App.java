f3f1009e96044fc2f6f520d0545b58bb74ff670c
mhunek <mhunek@seznam.cz>
2015-09-22 18:43:20 +0200

    Initial commit

---
+++ app/src/main/java/cz/muni/fi/pv256/movio/androidprojekt393640/App.java
@@ -0,0 +1,34 @@
+package cz.muni.fi.pv256.movio.androidprojekt393640;
+
+/**
+ * Created by mhunek on 21/9/2015.
+ */
+public class App  extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (BuildConfig.DEBUG) {
+            initStrictMode();
+        }
+    }
+
+    private void initStrictMode() {
+        StrictMode.ThreadPolicy.Builder tpb = new StrictMode.ThreadPolicy.Builder()
+                .detectAll()
+                .penaltyLog();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            tpb.penaltyFlashScreen();
+        }
+        StrictMode.setThreadPolicy(tpb.build());
+
+        StrictMode.VmPolicy.Builder vmpb = new StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .penaltyLog();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            vmpb.detectLeakedClosableObjects();
+        }
+        StrictMode.setVmPolicy(vmpb.build());
+    }
+}
